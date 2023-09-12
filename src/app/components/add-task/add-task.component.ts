ba56926116b6e1b623319bb98c06406bc002b26e
Krzysztof <ks.staszewski@gmail.com>
2023-09-12 18:51:02 +0200

    ui toggle

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,5 +1,7 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/data/tasks/task.type';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-task',
@@ -10,6 +12,14 @@ export class AddTaskComponent {
   text: string = '';
   day: string = '';
   reminder: boolean = false;
+  subscription: Subscription;
+  showForm: boolean = false;
+
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe((value) => {
+      this.showForm = value;
+    });
+  }
 
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
 
@@ -19,7 +29,7 @@ export class AddTaskComponent {
     const newTask: Task = {
       text: this.text,
       day: this.day,
-      reminder: this.reminder
+      reminder: this.reminder,
     };
 
     this.onAddTask.emit(newTask);
